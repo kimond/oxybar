ed42d82b0ba1e4f4568206eee8c4ce22363cca8d
Kim Desrosiers <kimdesro@gmail.com>
2018-06-13 08:43:29 -0400

    Relm version with working App bt not working block.

--- src/block.rs
+++ src/block.rs
@@ -1,61 +1,90 @@
-use glib;
-use std::cell::RefCell;
-use std::sync::mpsc::{channel, Receiver};
 use std::thread;
 use std::time::Duration;
 use sys_info::loadavg;
-use gtk::{Box, Label, LabelExt};
+use gtk::{Label, LabelExt};
+use relm::Channel;
+use relm::Update;
+use relm::Relm;
+use relm::Widget;
+
+pub struct Model {
+    _channel: Channel<String>,
+    value: String,
+}
+
+#[derive(Msg)]
+pub enum Msg {
+    Value(String)
+}
 
 pub struct CpuModule {
+    model: Model,
     pub label: Label
 }
 
-impl CpuModule {
-    pub fn new() -> CpuModule {
-        let label = Label::new("...");
+impl Update for CpuModule {
+    type Model = Model;
+    type ModelParam = ();
+    type Msg = Msg;
 
-        CpuModule { label }
-    }
-
-    pub fn spawn_polling(self) {
-        let (tx, rx) = channel();
-        GLOBAL.with(|global| {
-            *global.borrow_mut() = Some((self.label.clone(), rx))
+    fn model(relm: &Relm<Self>, _: ()) -> Model {
+        let stream = relm.stream().clone();
+        let (channel, sender) = Channel::new(move |val| {
+            println!("in stream");
+            stream.emit(Msg::Value(val));
         });
-
         thread::spawn(move || {
             loop {
                 thread::sleep(Duration::from_secs(1));
                 match loadavg() {
                     Ok(load) => {
                         let load_value = load.one.to_string();
-                        println!("{}", load_value);
-                        tx.send(load_value).expect("Counld't send data to channel");
-                        glib::idle_add(receive);
+                        sender.send(load_value).expect("Counld't send data to channel");
                     }
                     Err(x) => {
                         eprintln!("Cannot load cpu usage: {}", x);
-                        tx.send("error".to_string()).expect("Couldn't send data to channel");
-                        glib::idle_add(receive);
+                        sender.send("error".to_string()).expect("Couldn't send data to channel");
                     }
                 }
             }
         });
+        Model {
+            _channel: channel,
+            value: "...".to_string(),
+        }
+    }
+
+    fn update(&mut self, event: Msg) {
+        println!("in update");
+        match event {
+            Msg::Value(val) => {
+                println!("{}", &val);
+                &self.label.set_text(&val);
+            },
+        }
     }
 }
 
-fn receive() -> glib::Continue {
-    GLOBAL.with(|global| {
-        if let Some((ref label, ref rx)) = *global.borrow() {
-            if let Ok(text) = rx.try_recv() {
-                label.set_text(&text);
-            }
-        }
-    });
-    glib::Continue(false)
+impl Widget for CpuModule {
+    type Root = Label;
+
+    fn root(&self) -> Self::Root {
+        self.label.clone()
+    }
+
+
+    fn view(_relm: &Relm<Self>, model: Self::Model) -> Self {
+        let label = Label::new("...");
+
+        label.set_text(&model.value);
+
+        let mut module = CpuModule {
+            model,
+            label
+        };
+
+        module
+    }
 }
 
-// declare a new thread local storage key
-thread_local!(
-    static GLOBAL: RefCell<Option<(Label, Receiver<String>)>> = RefCell::new(None)
-);
+
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ extern crate gdk;
 extern crate glib;
 extern crate gtk;
 extern crate sys_info;
+#[macro_use]
+extern crate relm;
+#[macro_use]
+extern crate relm_derive;
+
+use relm::{Relm, Widget, Update};
 
 mod block;
 
 use gdk::prelude::*;
 use gtk::{Box, Label, Window, WindowType};
 use gtk::prelude::*;
-use block::{CpuModule};
+use relm::init;
+use block::CpuModule;
 
 
 pub struct Bar {
@@ -28,9 +35,8 @@ impl Bar {
 
         left_widgets.pack_start(&test_label, true, true, 0);
 
-        let cpu_module = CpuModule::new();
-        right_widgets.pack_start(&cpu_module.label, true, true, 0);
-        cpu_module.spawn_polling();
+        let cpu_module = init::<CpuModule>(()).unwrap();
+        right_widgets.pack_start(cpu_module.widget(), true, true, 0);
 
         let container = gtk::Box::new(gtk::Orientation::Horizontal, 0);
 
@@ -43,13 +49,46 @@ impl Bar {
 
 const CSS: &str = include_str!("../styles/app.css");
 
-pub struct App {
-    pub window: Window,
-    pub bar: Bar,
+struct Model {}
+
+#[derive(Msg)]
+enum Msg {
+    Quit,
 }
 
-impl App {
-    fn new() -> App {
+struct App {
+    model: Model,
+    window: Window,
+    bar: Bar,
+}
+
+impl Update for App {
+    type Model = Model;
+    type ModelParam = ();
+    type Msg = Msg;
+
+    fn model(_: &Relm<Self>, _: ()) -> Model {
+        Model {
+        }
+    }
+
+    fn update(&mut self, event: Msg) {
+        match event {
+            Msg::Quit => gtk::main_quit(),
+        }
+    }
+
+}
+
+impl Widget for App {
+    type Root = Window;
+
+    fn root(&self) -> Self::Root {
+        self.window.clone()
+    }
+
+    // Create the widgets.
+    fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
         let display = gdk::Display::get_default().expect("Unable to get default display");
         let monitor = display.get_primary_monitor().expect("Unable to get monitor");
         let monitor_rec = monitor.get_geometry();
@@ -68,25 +107,23 @@ impl App {
         window.set_type_hint(gdk::WindowTypeHint::Dock);
         window.get_style_context().map(|c| c.add_class("oxybar-window"));
 
-        window.connect_delete_event(|_, _| {
-            gtk::main_quit();
-            Inhibit(false)
-        });
+        // Connect the signal `delete_event` to send the `Quit` message.
+        connect!(relm, window, connect_delete_event(_, _), return (Some(Msg::Quit), Inhibit(false)));
 
         let bar = Bar::new();
 
         window.add(&bar.container);
 
-        App { window, bar }
+        window.show_all();
+
+        App {
+            model,
+            window,
+            bar
+        }
     }
 }
 
 fn main() {
-    gtk::init().expect("Oxybar application initialization failed...");
-
-    let app = App::new();
-
-    app.window.show_all();
-
-    gtk::main();
+    App::run(()).unwrap();
 }
